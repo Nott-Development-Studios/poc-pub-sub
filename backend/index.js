6e0f903bef5514d81a75652bc85d1c1e38e45793
Robert Moritz <novasykst@gmail.com>
2021-03-08 20:56:16 +0000

    synchronous pull

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,48 @@
-const { PubSub } = require("@google-cloud/pubsub");
+const { v1 } = require("@google-cloud/pubsub");
 
 require("dotenv").config();
 
+const projectId = "poc-pub-sub-1615149193238";
 const subscriptionName = "my-sub";
-const timeout = 60;
 
-const pubSubClient = new PubSub();
+// Creates a client; cache this for further use.
+const subClient = new v1.SubscriberClient();
 
-function listenForMessages() {
-  // References an existing subscription
-  const subscription = pubSubClient.subscription(subscriptionName);
+async function synchronousPull() {
+  const formattedSubscription = subClient.subscriptionPath(
+    projectId,
+    subscriptionName
+  );
 
-  // Create an event handler to handle messages
-  let messageCount = 0;
-  const messageHandler = (message) => {
-    console.log(`Received message ${message.id}:`);
-    console.log(`\tData: ${message.data}`);
-    console.log(`\tAttributes: ${message.attributes}`);
-    messageCount += 1;
-
-    // "Ack" (acknowledge receipt of) the message
-    message.ack();
+  // The maximum number of messages returned for this request.
+  // Pub/Sub may return fewer than the number specified.
+  const request = {
+    subscription: formattedSubscription,
+    maxMessages: 1,
   };
 
-  // Listen for new messages until timeout is hit
-  subscription.on("message", messageHandler);
+  // The subscriber pulls a specified number of messages.
+  const [response] = await subClient.pull(request);
 
-  // setTimeout(() => {
-  //   subscription.removeListener("message", messageHandler);
-  //   console.log(`${messageCount} message(s) received.`);
-  // }, timeout * 1000);
+  // Process the messages.
+  const ackIds = [];
+  for (const message of response.receivedMessages) {
+    console.log(`Received message: ${message.message.data}`);
+    ackIds.push(message.ackId);
+  }
+
+  if (ackIds.length !== 0) {
+    // Acknowledge all of the messages. You could also ackknowledge
+    // these individually, but this is more efficient.
+    const ackRequest = {
+      subscription: formattedSubscription,
+      ackIds: ackIds,
+    };
+
+    await subClient.acknowledge(ackRequest);
+  }
+
+  console.log("Done.");
 }
 
-listenForMessages();
+synchronousPull().catch(console.error);
